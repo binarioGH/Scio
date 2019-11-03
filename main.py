7e0a523b5f5e0f1dd6d3759bbd353fe9162056fe
binarioGH <lacuentafalsadediegojonas@gmail.com>
2019-11-02 22:47:12 -0700

    new buttons

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from tkinter import *
 from json import loads
 from codecs import open as copen
 from random import choice
+from pyperclip import copy
 
 WIDTH = 700
 HEIGHT= 400 
@@ -27,19 +28,26 @@ class Worder:
 			return -1
 		else:
 			self.content = content
-	def check_translation(self, word, answare):
-		if self.content:
-			if answare in self.content[word]:
+	def check_translation(self,answare):
+		if self.content != 0:
+			if answare.lower() in self.content[self.label["text"]]:
+				self.newWord()
 				return 1
-			return 0
-		return -1
+			else:
+				#print(":(")
+				return 0
+		else:
+			#print("bruh!")
+			return -1
 
 	def newWord(self):
 		current = self.label["text"]
 		newword = choice(self.wordlist)
+		#print("owo")
 		while current == newword:
 		    newword = choice(self.wordlist)
-		self.label["text"] = newword		
+		#print("uwu")
+		self.label.config(text=newword)		
 
 def main():
 	root = Tk()
@@ -47,12 +55,20 @@ def main():
 	root.geometry("{}x{}".format(WIDTH, HEIGHT))
 	mainpage = Frame(root, bg=GREEN)
 	mainpage.place(relx=0, rely=0, relwidth=1, relheight=1)
-	console = Canvas(mainpage,bg=GREEN)
-	console.place(relx=0, rely=0, relwidth=0.3, relheight=1.1)
 	questions = Canvas(mainpage, bg=SILVER)
 	questions.place(relx=0.3, rely=0, relwidth=0.7, relheight=1)
 	wordquestion = Worder(questions, "source\\esperanto-english.json")
 	wordquestion.label.place(relx=0, rely=0, relwidth=1, relheight=0.05)
+	console = Canvas(mainpage,bg=GREEN)
+	console.place(relx=0, rely=0, relwidth=0.4, relheight=1.1)
+	answare = Entry(console, font=("Courier", 16))
+	answare.place(relx=0.1, rely=0.1, relwidth=0.8, relheight=0.05)
+	send = Button(console, text="Check!", font=("Courier", 16), command=lambda: wordquestion.check_translation(answare.get()))
+	send.place(relx=0.1, rely=0.17, relwidth=0.3, relheight=0.05)
+	copytext = Button(console, text="Copy!", font=("Courier", 16), command=lambda: copy(wordquestion.label["text"]))
+	copytext.place(relx=0.1, rely=0.24, relwidth=0.3, relheight=0.05)
+	newwbutton = Button(console, text="NEW WORD", font=("Courier", 11), command=lambda: wordquestion.newWord())
+	newwbutton.place(relx=0.5, rely=0.17, relwidth=0.3, relheight=0.05)
 	root.mainloop()
 
 
