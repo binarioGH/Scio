b2ed271f61b863df14ac9cd9c5fc4f0c68954eb3
binarioGH <lacuentafalsadediegojonas@gmail.com>
2020-01-19 22:44:14 -0800

    changing the colors

--- main.py
+++ main.py
@@ -11,23 +11,25 @@ def sound(file):
 	PlaySound(file, SND_ASYNC)
 
 
-WIDTH = 700
-HEIGHT= 400 
-BLUE = "#2E2EFE"
-GREEN = "#00FF80"
-SILVER = "#ECF0F1"
-YELLOW = "#F7FE2E"
-RED = "#DF013A"
-
+class WindowConfiguration:
+	def __init__(self):
+		self.WIDTH = 700
+		self.HEIGHT = 400
+		self.MAIN_COLOR = "#00FF80"
+		self.SECONDARY_COLOR = "#ECF0F1"
+		self.WRONG = "#DF013A"
+		self.GOOD = "#00FF80"
+		self.ADD_WORD = "F7FE2E" 
+wc = WindowConfiguration()
 class Worder:
 	def __init__(self,root, file="", answares="" ,open_file=True, fnt=("Courier", 15)):
-		self.label = Label(root, bg=SILVER, font=fnt, fg="black", justify="left")
+		self.label = Label(root, bg=wc.SECONDARY_COLOR, font=fnt, fg="black", justify="left")
 		self.answares = answares
 		self.file = file
 		self.content = 0
 		self.root = root
 		self.pointc = 0
-		self.points = Label(root, bg=SILVER, font=fnt, fg=RED, text="Points: {}".format(self.pointc))
+		self.points = Label(root, bg=wc.SECONDARY_COLOR, font=fnt, fg=wc.WRONG, text="Points: {}".format(self.pointc))
 		#self.dictionary = 0
 		self.wordlist = []
 		if open_file:
@@ -44,10 +46,10 @@ class Worder:
 				content = loads(f.read())
 		except Exception as e: 
 			if changing:
-				label.config(fg=RED)
+				label.config(fg=wc.WRONG)
 				sound("source\\wrong.wav")
 				if border != "":
-					border.config(bg=RED)
+					border.config(bg=wc.WRONG)
 					self.root.update()
 					sleep(0.2)
 					border.config(bg="black")
@@ -60,7 +62,7 @@ class Worder:
 				self.wordlist = list(self.content["Language"])
 			if changing:
 				if border != "":
-					border.config(bg=GREEN)
+					border.config(bg=wc.GOOD)
 					self.root.update()
 					sound("source\\correct.wav")
 					sleep(0.2)
@@ -78,20 +80,20 @@ class Worder:
 		if self.content != 0:
 			if answare.lower() in self.content["Language"][self.label["text"]]:
 				sound("source\\correct.wav")
-				self.label.config(bg=GREEN)
+				self.label.config(bg=wc.GOOD)
 				self.root.update()
 				sleep(0.4)
-				self.label.config(bg=SILVER)
+				self.label.config(bg=wc.SECONDARY_COLOR)
 				self.newWord()
 				entry.delete(0, END)
 				self.pointc += 1
 				self.update_points()
 			else:
 				sound("source\\wrong.wav")
-				self.label.config(bg=RED)
+				self.label.config(bg=wc.WRONG)
 				self.root.update()
 				sleep(0.4)
-				self.label.config(bg=SILVER)
+				self.label.config(bg=wc.SECONDARY_COLOR)
 				#print(":(")
 				self.pointc -= 1
 				self.update_points()
@@ -147,7 +149,7 @@ class Worder:
 				ans = ans.strip()
 				if delete and not ans in self.content["Language"][word]:
 					return
-				self.label.config(bg=YELLOW)
+				self.label.config(bg=wc.ADD_WORD)
 				sound("source\\added.wav")
 				self.root.update()
 				if delete:
@@ -156,7 +158,7 @@ class Worder:
 					self.content["Language"][word].append(ans)
 				self.getansw()
 				sleep(0.4)
-				self.label.config(bg=SILVER)
+				self.label.config(bg=wc.SECONDARY_COLOR)
 				self.root.update()
 				with open(self.file, "w", encoding="utf-8") as f:
 					f.write(dumps(self.content, indent=4))
@@ -165,20 +167,21 @@ class Worder:
 
 
 def main():
+	
 	root = Tk()
 	root.title("Scio")
-	root.geometry("{}x{}".format(WIDTH, HEIGHT))
-	change_json = Frame(root, bg=SILVER)
+	root.geometry("{}x{}".format(wc.WIDTH, wc.HEIGHT))
+	change_json = Frame(root, bg=wc.SECONDARY_COLOR)
 	change_json.place(relx=0,rely=0,relwidth=1, relheight=1)
-	dictionary = Frame(root, bg=SILVER)
+	dictionary = Frame(root, bg=wc.SECONDARY_COLOR)
 	dictionary.place(relx=0,rely=0,relwidth=1,relheight=1)
-	mainpage = Frame(root, bg=GREEN)
+	mainpage = Frame(root, bg=wc.MAIN_COLOR)
 	mainpage.place(relx=0, rely=0, relwidth=1, relheight=1)
-	back = Button(dictionary, text="<-", bg=RED, fg="white" ,font=("Courier", 13), command=lambda: mainpage.tkraise())
+	back = Button(dictionary, text="<-", bg=wc.WRONG, fg="white" ,font=("Courier", 13), command=lambda: mainpage.tkraise())
 	back.place(relx=0.05, rely=0.05, relwidth=0.1, relheight=0.05)
-	questions = Canvas(mainpage, bg=SILVER)
+	questions = Canvas(mainpage, bg=wc.SECONDARY_COLOR)
 	questions.place(relx=0.3, rely=0, relwidth=0.8, relheight=1)
-	promp = Label(questions, bg=SILVER, font=("Courier", 18))
+	promp = Label(questions, bg=wc.SECONDARY_COLOR, font=("Courier", 18))
 	promp["text"] = "Translate this: "
 	promp.place(relx=0, rely=0.01, relwidth=0.7, relheight=0.4)
 	answarebox = Listbox(questions, bg="white", font=("Courier", 14))
@@ -196,13 +199,13 @@ def main():
 	dicscroll.config(command=words.yview)
 	wordsearcher = Entry(dictionary, font=("Courier", 18))
 	wordsearcher.place(relx=0.2, rely=0.2, relwidth=0.4, relheight=0.1)
-	search = Button(dictionary, bg=GREEN, fg="black", text="Search",command=lambda:wordquestion.getansw(words, wordsearcher.get(), True))
+	search = Button(dictionary, bg=wc.MAIN_COLOR, fg="black", text="Search",command=lambda:wordquestion.getansw(words, wordsearcher.get(), True))
 	search.place(relx=0.65,rely=0.22, relwidth=0.15, relheight=0.05)
 	
 	#wordquestion.dictionary = words
 	wordquestion.label.place(relx=0, rely=0.3, relwidth=0.99, relheight=0.050)
 	wordquestion.points.place(relx=0.3, rely=0.9, relwidth=0.4, relheight=0.08)
-	console = Canvas(mainpage,bg=GREEN)
+	console = Canvas(mainpage,bg=wc.MAIN_COLOR)
 	console.place(relx=0, rely=0, relwidth=0.4, relheight=1.1)
 	answare = Entry(console, font=("Courier", 16))
 	answare.place(relx=0.1, rely=0.1, relwidth=0.8, relheight=0.05)
@@ -216,24 +219,24 @@ def main():
 	getans.place(relx=0.5, rely=0.24, relwidth=0.3, relheight=0.05)
 	newans = Button(console, text="Add Answare", font=("Courier", 18), command=lambda: wordquestion.addNewAnsware(answare.get()))
 	newans.place(relx=0.1, rely=0.75, relwidth=0.7, relheight=0.05)
-	searchw = Button(console, bg=RED, fg="white",text="Search a word", font=("Courier", 14), command=lambda:dictionary.tkraise())
+	searchw = Button(console, bg=wc.WRONG, fg="white",text="Search a word", font=("Courier", 14), command=lambda:dictionary.tkraise())
 	searchw.place(relx=0.1, rely=0.31, relwidth=0.7, relheight=0.05)
 	delans = Button(console, text="Delete Answare", font = ("Courier", 18), command=lambda: wordquestion.addNewAnsware(answare.get(), True))
 	delans.place(relx=0.1, rely=0.83, relwidth=0.7, relheight=0.05)
 	change_jsonb = Button(questions, text="...", command=lambda:change_json.tkraise())
 	change_jsonb.place(relx=0.75, rely=0.9, relwidth=0.1, relheight=0.08)
-	jback = Button(change_json, bg=RED, fg="white", font=("Courier", 18), text="<-", command=lambda: mainpage.tkraise())
+	jback = Button(change_json, bg=wc.WRONG, fg="white", font=("Courier", 18), text="<-", command=lambda: mainpage.tkraise())
 	jback.place(relx=0.05, rely=0.05, relwidth=0.05, relheight=0.05)
 	sborder = Label(change_json, bg="black")
 	sborder.place(relx=0.24, rely=0.43, relwidth=0.58, relheight=0.14)
-	disclaim = Label(change_json, font=("Courier", 14), text="Write the path of the new file:", bg=SILVER, fg="black", anchor="w")
+	disclaim = Label(change_json, font=("Courier", 14), text="Write the path of the new file:", bg=wc.SECONDARY_COLOR, fg="black", anchor="w")
 	disclaim.place(relx=0.24, rely=0.3, relwidth=0.5, relheight=0.1)
 	search_file = Entry(change_json, font=("Courier", 18))
 	search_file.place(relx=0.25, rely=0.45, relwidth=0.4, relheight=0.1)
-	errorLabel = Label(change_json, font=("Courier", 12), bg=SILVER, fg=RED, text="")
+	errorLabel = Label(change_json, font=("Courier", 12), bg=wc.SECONDARY_COLOR, fg=wc.WRONG, text="")
 	errorLabel.place(relx=0, rely=0.6, relwidth=1, relheight=0.14)	
 
-	ssearch = Button(change_json, font=("Courier", 18), text="Change!", bg=RED, fg="white", command=lambda:wordquestion.load_file(search_file.get(), True, errorLabel, sborder))
+	ssearch = Button(change_json, font=("Courier", 18), text="Change!", bg=wc.WRONG, fg="white", command=lambda:wordquestion.load_file(search_file.get(), True, errorLabel, sborder))
 	ssearch.place(relx=0.65, rely=0.45, relwidth=0.16, relheight=0.1)
 	root.mainloop()
 
